51310d260af68b91294d4f4101e7e1aebd0cf8f3
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-09-17 23:02:20 +0530

    signin/signup component done

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -1,25 +1,91 @@
-import React, { Component } from 'react'
-import "./signin.css"
+import React, { Component } from "react";
+import "./signin.css";
 export default class Signin extends Component {
+  state = {
+    "signinEmail": "",
+    "signinPassword": "",
+    "signupName": "",
+    "signupEmail": "",
+    "signupPassword": "",
+  };
+  toggleForms = (event) => {
+    document.body.classList.toggle("toggle-forms");
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id] : [event.target.value]
+    })
+  };
   render() {
     return (
       <div className="signin-container">
-        <div className="flag signin">
+        <div className="flag signin" onClick={this.toggleForms}>
           <h1>Signin</h1>
         </div>
-        <div className="flag signup">
+        <div className="flag signup" onClick={this.toggleForms}>
           <h1>Signup</h1>
         </div>
         <div className="signin-form">
           <form action="">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="" id="email" required />
+            <label htmlFor="email" id="signin-email-label">
+              Email
+            </label>
+            <input
+              onChange={this.handleChange}
+              type="email"
+              name="email"
+              id="signinEmail"
+              value={this.state.signinEmail}
+              required
+            />
             <label htmlFor="password">Password</label>
-            <input type="text" name="" id="password" required />
+            <input
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+              id="signinPassword"
+              value={this.state.signinPassword}
+              required
+            />
             <button>Submit</button>
           </form>
         </div>
-        <div className="signup-form"></div>
+        <div className="signup-form">
+          <form action="">
+            <label htmlFor="name" id="signup-name-label">
+              Name
+            </label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              id="signupName"
+              value={this.state.signupName}
+              required
+            />
+            <label htmlFor="email" id="signup-email-label">
+              Email
+            </label>
+            <input
+              onChange={this.handleChange}
+              type="email"
+              name="email"
+              id="signupEmail"
+              value={this.state.signupEmail}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+              id="signupPassword"
+              value={this.state.signupPassword}
+              required
+            />
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
